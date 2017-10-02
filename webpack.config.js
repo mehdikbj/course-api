7cb0f108af09b68146eb6b7ae3cdc3167c6c1a29
mehdi kabbaj <mka@octo.com>
2017-10-02 11:02:08 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const PATHS = {
+    output: path.join(__dirname, './target/classes/static')
+};
+
+module.exports = {
+    entry: [
+        './src/main/scripts/main.js',
+    ],
+    output: {
+        path: PATHS.output,
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', './src/main/scripts'],
+        extensions: ['', '.js', '.jsx']
+    },
+};
